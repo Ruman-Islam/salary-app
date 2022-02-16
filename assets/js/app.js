53465c22299b2cc2e1642ba6b7c73928f0736374
Ruman-Islam <rumanislam48@gmail.com>
2022-02-17 03:36:17 +0600

    calculate part error handle done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-const calculateButton = document.querySelector('#calculate');
+const calculateButton = document.querySelector('#calculate-button');
+const savingsButton = document.querySelector('#savings-button');
 const incomeInput = document.querySelector('#income-input');
 const foodInput = document.querySelector('#food-input');
 const rentInput = document.querySelector('#rent-input');
@@ -6,13 +7,48 @@ const clothesInput = document.querySelector('#clothes-input');
 const savingsInput = document.querySelector('#savings-input');
 const totalExpensesField = document.querySelector('#total-expenses');
 const balance = document.querySelector('#balance');
+
+
 calculateButton.addEventListener('click', function () {
     const incomeAmount = parseFloat(incomeInput.value);
     const foodAmount = parseFloat(foodInput.value);
-    const rentAmount = parseFloat(foodInput.value);
-    const clothesAmount = parseFloat(foodInput.value);
-    const totalExpenses = foodAmount + rentAmount + clothesAmount;
-    const remainingBalance = incomeAmount - totalExpenses;
-    totalExpensesField.innerText = totalExpenses;
-    balance.innerText = remainingBalance;
+    const rentAmount = parseFloat(rentInput.value);
+    const clothesAmount = parseFloat(clothesInput.value);
+    if (isNaN(incomeAmount) || incomeAmount < 0) {
+        document.querySelector('#income-error-field').innerText = 'Enter any number';
+        totalExpensesField.innerText = '';
+        balance.innerText = '';
+    } else if (isNaN(foodAmount) || foodAmount < 0) {
+        document.querySelector('#food-error-field').innerText = 'Enter any number';
+        totalExpensesField.innerText = '';
+        balance.innerText = '';
+    } else if (isNaN(rentAmount) || rentAmount < 0) {
+        document.querySelector('#rent-error-field').innerText = 'Enter any number';
+        totalExpensesField.innerText = '';
+        balance.innerText = '';
+    } else if (isNaN(clothesAmount) || clothesAmount < 0) {
+        document.querySelector('#clothes-error-field').innerText = 'Enter any number';
+        totalExpensesField.innerText = '';
+        balance.innerText = '';
+    } else {
+        const totalExpenses = foodAmount + rentAmount + clothesAmount;
+        const remainingBalance = incomeAmount - totalExpenses;
+        if (totalExpenses > incomeAmount) {
+            totalExpensesField.innerText = totalExpenses;
+            balance.innerText = 'Over expenses';
+        } else {
+            totalExpensesField.innerText = totalExpenses;
+            balance.innerText = remainingBalance;
+        }
+        document.querySelector('#income-error-field').innerText = '';
+        document.querySelector('#food-error-field').innerText = '';
+        document.querySelector('#rent-error-field').innerText = '';
+        document.querySelector('#clothes-error-field').innerText = '';
+    }
+});
+
+
+savingsButton.addEventListener('click', function () {
+    const savingsAmount = parseFloat(savingsInput.value);
+
 });
