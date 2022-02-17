98b2974073fbdedfa9beb4a2eb6fc6ef1f781213
Ruman-Islam <rumanislam48@gmail.com>
2022-02-17 22:41:43 +0600

    code organized done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,72 +1,109 @@
-// Utility functions Start //
-// parseFloat blending function
+//.. Utility functions Start ..//
+
+// parseFloat blending function //
 function parseBlender(id) {
     const inputField = document.querySelector(id);
     return parseFloat(inputField.value);
 }
+/* ..................................*/
+/* ..................................*/
 
-// Function of empty inner texts
-function setInnerTextEmpty(idOne, idTwo, idThree, idFour) {
+// Function to Empty Inner Texts //
+function setInnerTextEmpty(idOne, idTwo, idThree, idFour) {  // This is a very much dynamic function to set value at same time //
     document.querySelector(idOne).innerText = '';
     document.querySelector(idTwo).innerText = '';
     document.querySelector(idThree).innerText = '';
     document.querySelector(idFour).innerText = '';
 }
+/* ..................................*/
+/* ..................................*/
 
-// Function of set any kind of value as inner text
+// Function to Set any kind of value as inner text //
 function setInnerText(id, value) {
     document.querySelector(id).innerText = value;
 }
-// Utility functions End //
+/* ..................................*/
+/* ..................................*/
+
+//.... Utility Functions End.... //
 
 
-// Total expenses calculation
+// ....Total Expenses Calculation... //
 function expenseCalculator() {
     const incomeAmount = parseBlender('#income-input');
     const foodAmount = parseBlender('#food-input');
     const rentAmount = parseBlender('#rent-input');
     const clothesAmount = parseBlender('#clothes-input');
     if (isNaN(incomeAmount) || incomeAmount < 0) {
-        setInnerText('#income-error-field', 'Enter any number');
+        if (incomeAmount < 0) {
+            setInnerText('#income-error-field', 'Negative number');
+        } else {  // validation
+            setInnerText('#income-error-field', 'Invalid input');
+        }
     } else if (isNaN(foodAmount) || foodAmount < 0) {
-        setInnerText('#food-error-field', 'Enter any number');
+        if (foodAmount < 0) {
+            setInnerText('#food-error-field', 'Negative number');
+        } else {  // validation
+            setInnerText('#food-error-field', 'Invalid input');
+        }
     } else if (isNaN(rentAmount) || rentAmount < 0) {
-        setInnerText('#rent-error-field', 'Enter any number');
+        if (rentAmount < 0) {
+            setInnerText('#rent-error-field', 'Negative number');
+        } else {  // validation
+            setInnerText('#rent-error-field', 'Invalid input');
+        }
     } else if (isNaN(clothesAmount) || clothesAmount < 0) {
-        setInnerText('#clothes-error-field', 'Enter any number');
+        if (clothesAmount < 0) {
+            setInnerText('#clothes-error-field', 'Negative number');
+        } else {  // validation
+            setInnerText('#clothes-error-field', 'Invalid input');
+        }
     } else {
         const totalExpenses = foodAmount + rentAmount + clothesAmount;
         if (totalExpenses > incomeAmount) {
             setInnerText('#balance', 'Over expenses');
-        } else {
+        } else {  // validation
             setInnerText('#balance', incomeAmount - totalExpenses);
         }
         setInnerText('#total-expenses', totalExpenses);
         setInnerTextEmpty('#income-error-field', '#food-error-field', '#rent-error-field', '#clothes-error-field');
     }
 }
+/* .......................................................................*/
+/* .......................................................................*/
 
-// Savings Calculation
+
+
+//.... Savings Calculation...... //
 function savingsCalculator() {
     const incomeAmount = parseBlender('#income-input');
     const savingsAmount = parseBlender('#savings-input');
-    const totalBalance = parseFloat(balance.innerText);
+    const totalBalance = parseFloat(balance.innerText);   // Directly captured from dom //
     const totalSavings = incomeAmount * (savingsAmount / 100);
     if (isNaN(savingsAmount) || savingsAmount < 0) {
-        setInnerText('#savings-error-field', 'Enter any number');
+        if (savingsAmount < 0) {
+            setInnerText('#savings-error-field', 'Negative number');
+        } else {  // validation
+            setInnerText('#savings-error-field', 'Invalid input');
+        }
     } else if (isNaN(incomeAmount) || incomeAmount < 0) {
-        setInnerText('#income-error-field', 'Enter any number');
-        setInnerTextEmpty('#savings-error-field', '#error-absorber', '#error-absorber', '#error-absorber');
+        if (incomeAmount < 0) {
+            setInnerText('#income-error-field', 'Negative number');
+            setInnerTextEmpty('#savings-error-field', '#error-absorber', '#error-absorber', '#error-absorber');
+        } else {  // validation
+            setInnerText('#income-error-field', 'Invalid input');
+            setInnerTextEmpty('#savings-error-field', '#error-absorber', '#error-absorber', '#error-absorber');
+        }
     } else {
         if (totalBalance < totalSavings) {
-            if (totalBalance === 0) {
+            if (totalBalance === 0) {  // validation
                 setInnerText('#balance', incomeAmount);
                 setInnerText('#savings-amount', totalSavings);
                 setInnerText('#remaining-balance', incomeAmount - totalSavings);
                 setInnerTextEmpty('#income-error-field', '#savings-error-field', '#error-absorber', '#error-absorber');
             } else {
                 setInnerText('#savings-amount', totalSavings);
-                setInnerText('#remaining-balance', 'Not enough money to save');
+                setInnerText('#remaining-balance', 'Not enough money');
                 setInnerTextEmpty('#income-error-field', '#error-absorber', '#error-absorber', '#error-absorber');
             }
         } else {
@@ -76,3 +113,5 @@ function savingsCalculator() {
         }
     }
 }
+/* .......................................................................*/
+/* .......................................................................*/
